b1c39c1ca5a7093e730aaa958feadcf7762386aa
Christian Pereira <christianpbsd@gmail.com>
2021-10-12 14:47:26 -0300

    Sistema Financas

---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import { Item } from '../types/Item'; 
+
+
+export const items: Item[] = [
+    { date: new Date (2021, 9, 20), category: 'food', title: 'Burguer King', value: 25.50 },
+
+    { date: new Date (2021, 10, 12), category: 'food', title: 'McDonalds', value: 40.50 },
+
+    { date: new Date (2021, 10, 5), category: 'trip', title: 'Santos', value: 100.00 },
+
+    { date: new Date (2021, 9, 4), category: 'rent', title: 'Aluguel', value: 2300.00 },
+
+    { date: new Date (2021, 9, 4), category: 'salary', title: 'Salário ACME', value: 15000.00 },
+
+];
