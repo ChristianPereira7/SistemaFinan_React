325e7df984245af8c3bfddcd04166974a52a3287
Christian Pereira <christianpbsd@gmail.com>
2021-10-14 05:16:46 -0300

    Table area categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './Components/TableArea';
+
 
 const App = () => {
+
+    const [list, setList] = useState(items);
+    const [filteredList, setFilteredList] = useState<Item[]>([]); 
+    const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+
+    useEffect(() => {
+
+      setFilteredList(filterListByMonth(list, currentMonth));
+
+    }, [list, currentMonth])
+
+
+
     return(
       <C.Container>
         <C.Header>
@@ -19,7 +41,7 @@ const App = () => {
 
            {/* Tabela de itens */}
 
-
+            <TableArea list={filteredList}/>
 
 
         </C.Body>
--- src/data/items.ts
+++ src/data/items.ts
@@ -2,14 +2,9 @@ import { Item } from '../types/Item';
 
 
 export const items: Item[] = [
-    { date: new Date (2021, 9, 20), category: 'food', title: 'Burguer King', value: 25.50 },
-
-    { date: new Date (2021, 10, 12), category: 'food', title: 'McDonalds', value: 40.50 },
-
-    { date: new Date (2021, 10, 5), category: 'trip', title: 'Santos', value: 100.00 },
-
-    { date: new Date (2021, 9, 4), category: 'rent', title: 'Aluguel', value: 2300.00 },
-
-    { date: new Date (2021, 9, 4), category: 'salary', title: 'Salário ACME', value: 15000.00 },
+    { date: new Date(2021, 9, 20), category: 'food', title: 'Burguer King', value: 25.50 },
+    { date: new Date(2021, 10, 12), category: 'food', title: 'McDonalds', value: 40.50 },
+    { date: new Date(2021, 9, 4), category: 'rent', title: 'Aluguel', value: 2300.00 },
+    { date: new Date(2021, 9, 4), category: 'salary', title: 'Salário ACME', value: 3000.00 },
 
 ];
