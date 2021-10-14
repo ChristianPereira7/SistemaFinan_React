10c1cd5b21710f06f91f2d8398bcbd1976c03f89
Christian Pereira <christianpbsd@gmail.com>
2021-10-14 08:05:04 -0300

    Resume area

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ const App = () => {
     const [list, setList] = useState(items);
     const [filteredList, setFilteredList] = useState<Item[]>([]); 
     const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
 
 
     useEffect(() => {
@@ -22,6 +24,23 @@ const App = () => {
       setFilteredList(filterListByMonth(list, currentMonth));
 
     }, [list, currentMonth]);
+
+    useEffect(()=> {
+      let incomeCount = 0;
+      let expenseCount = 0;
+
+      for(let i in filteredList) {
+        if(categories[filteredList[i].category].expense){
+          expenseCount += filteredList[i].value;
+        }else{
+          incomeCount += filteredList[i].value;
+        }
+      }
+
+      setIncome(incomeCount);
+      setExpense(expenseCount)
+
+    }, [filteredList]);
 
 
     const handleMonthChange = (newMonth: string) => {
@@ -42,14 +61,11 @@ const App = () => {
             <InfoArea 
             currentMonth={currentMonth}
             onMonthChange={handleMonthChange}
+            income={income}
+            expense={expense}
             />
 
            {/* Área de insercão */}
-
-
-
-
-           {/* Tabela de itens */}
 
             <TableArea list={filteredList}/>
 
